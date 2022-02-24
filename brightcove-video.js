2764c4816878dada05f3595f0b7193ad599a4512
ShatadruMaity <63687156+ShatadruMaity@users.noreply.github.com>
2022-02-25 01:51:31 +0530

    Update brightcove-video.js

--- brightcove-video.js
+++ brightcove-video.js
@@ -1,1 +1,8 @@
-<script src="https://assets.adobedtm.com/f817b207cd04/ea760ba43ec3/launch-ENb2cf8f610b9140c7a4613d7743e517e5-development.min.js" async></script>
+(function ($) {  
+    function loadAdobeLaunchCode() {
+    var httpProtocol = window.location.protocol;
+    var url = "//assets.adobedtm.com/f817b207cd04/ea760ba43ec3/launch-ENb2cf8f610b9140c7a4613d7743e517e5-development.min.js";
+    $.getScript(httpProtocol + url, function () {         
+        });
+    }  
+})(jQuery);
